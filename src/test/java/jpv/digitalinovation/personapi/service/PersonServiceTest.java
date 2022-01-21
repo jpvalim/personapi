863660f9fd14610f59613145248e9723058661b0
Jeff Valim <jpvalim@hotmail.com>
2022-01-21 11:33:07 -0300

    JUnit Test

---
+++ src/test/java/jpv/digitalinovation/personapi/service/PersonServiceTest.java
@@ -0,0 +1,52 @@
+package jpv.digitalinovation.personapi.service;
+
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import jpv.digitalinovation.personapi.dto.MessageResponseDTO;
+import jpv.digitalinovation.personapi.dto.PersonDTO;
+import jpv.digitalinovation.personapi.entities.Person;
+import jpv.digitalinovation.personapi.repositories.PersonRepository;
+import jpv.digitalinovation.personapi.services.PersonService;
+import jpv.digitalinovation.personapi.utils.PersonUtils;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = PersonUtils.createFakeDTO();
+        Person expectedSavedPerson = PersonUtils.createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+        MessageResponseDTO succesMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, succesMessage);
+    }
+
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
+                .build();
+    }
+}
+
---
+++ src/test/java/jpv/digitalinovation/personapi/utils/PhoneUtils.java
@@ -0,0 +1,27 @@
+package jpv.digitalinovation.personapi.utils;
+
+import jpv.digitalinovation.personapi.dto.PhoneDTO;
+import jpv.digitalinovation.personapi.entities.Phone;
+import jpv.digitalinovation.personapi.enums.PhoneType;
+
+public class PhoneUtils {
+
+    private static final String PHONE_NUMBER = "1199999-9999";
+    private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+    private static final long PHONE_ID = 1L;
+
+    public static PhoneDTO createFakeDTO() {
+        return PhoneDTO.builder()
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+    public static Phone createFakeEntity() {
+        return Phone.builder()
+                .id(PHONE_ID)
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+}
